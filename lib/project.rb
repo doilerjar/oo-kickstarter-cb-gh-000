2f892ba3312fe0ac7f31b62d3f7324ded4465492
Kevin K <doilerjar@outlook.com>
2018-08-11 09:05:51 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,7 +9,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    if !backer.backed_project.include?(self)
+    if !backer.backed_projects.include?(self)
       backer.back_project(self)
     end
   end 
