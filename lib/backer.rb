b2021c9cabe2dd44e0e73b9be9ad38ce5c6c4608
Kevin K <doilerjar@outlook.com>
2018-08-11 08:46:19 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,19 @@
 class Backer
   
+  attr_accessor :projects
+  
+  attr_reader :name
+  
+  def initialize(name="")
+      @name = name
+      @projects = []
+  end 
+  
+  def back_project(project)
+    @projects << project
+  end 
+  
+  def backed_projects
+    @projects
+  end 
 end 
